ab5d264e5b2d907ab30a30190522f46dfd2af6f0
Michael Lange <dingoeatingfuzz@gmail.com>
2021-01-08 09:39:31 -0800

    fmt

--- builder/builder.go
+++ builder/builder.go
@@ -2,23 +2,23 @@ package builder
 
 import (
 	"context"
-  "strings"
-  "io/ioutil"
-  "os"
-  "os/exec"
-  "path"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
 
+	PackageJSON "github.com/cloudrecipes/packagejson"
 	"github.com/hashicorp/waypoint-plugin-sdk/terminal"
-  PackageJSON "github.com/cloudrecipes/packagejson"
 )
 
 type BuildConfig struct {
-	Target  string `hcl:"target"`
-	Options  []string `hcl:"options,optional"`
-  Output   string `hcl:"output,optional"`
-  OutPath  string `hcl:"out_path,optional"`
-  Build    bool `hcl:"build,optional"`
-  Public   bool `hcl:"public,optional"`
+	Target  string   `hcl:"target"`
+	Options []string `hcl:"options,optional"`
+	Output  string   `hcl:"output,optional"`
+	OutPath string   `hcl:"out_path,optional"`
+	Build   bool     `hcl:"build,optional"`
+	Public  bool     `hcl:"public,optional"`
 }
 
 type Builder struct {
@@ -30,9 +30,9 @@ func (b *Builder) Config() (interface{}, error) {
 }
 
 func (b *Builder) ConfigSet(config interface{}) error {
-  // All config options are optional.
-  // Any bad values are handled by pkg itself.
-  // Maybe in the future some static checking can be done here to speed things up, idk.
+	// All config options are optional.
+	// Any bad values are handled by pkg itself.
+	// Maybe in the future some static checking can be done here to speed things up, idk.
 
 	return nil
 }
@@ -70,72 +70,72 @@ func (b *Builder) build(ctx context.Context, ui terminal.UI) (*Binary, error) {
 	defer u.Close()
 	u.Update("Building application")
 
-  // Always run using the local package.json
-  // Enforce a single target (and therefore a single binary)
-  args := []string{".", "--targets", b.config.Target}
+	// Always run using the local package.json
+	// Enforce a single target (and therefore a single binary)
+	args := []string{".", "--targets", b.config.Target}
 
-  // Flags
-  if b.config.Build {
-    args = append(args, "--build")
-  }
-  if b.config.Public {
-    args = append(args, "--public")
-  }
+	// Flags
+	if b.config.Build {
+		args = append(args, "--build")
+	}
+	if b.config.Public {
+		args = append(args, "--public")
+	}
 
-  // Outputs
-  if b.config.Output != "" {
-    args = append(args, "--output")
-    args = append(args, b.config.Output)
-  }
-  if b.config.OutPath != "" {
-    args = append(args, "--out-path")
-    args = append(args, b.config.OutPath)
-  }
+	// Outputs
+	if b.config.Output != "" {
+		args = append(args, "--output")
+		args = append(args, b.config.Output)
+	}
+	if b.config.OutPath != "" {
+		args = append(args, "--out-path")
+		args = append(args, b.config.OutPath)
+	}
 
-  // Options
-  if b.config.Options != nil {
-    args = append(args, "--options")
-    args = append(args, strings.Join(args, ","))
-  }
+	// Options
+	if b.config.Options != nil {
+		args = append(args, "--options")
+		args = append(args, strings.Join(args, ","))
+	}
 
-  c := exec.Command("pkg", args...)
+	c := exec.Command("pkg", args...)
 
-  pwd, _ := os.Getwd()
-  u.Step(terminal.StatusWarn, pwd)
-  u.Step(terminal.StatusWarn, strings.Join(args, " "))
+	pwd, _ := os.Getwd()
+	u.Step(terminal.StatusWarn, pwd)
+	u.Step(terminal.StatusWarn, strings.Join(args, " "))
 
-  err := c.Run()
-  if err != nil {
-    u.Step(terminal.StatusError, "Pkg failed to build the binary.")
-    return nil, err
-  }
+	err := c.Run()
+	if err != nil {
+		u.Step(terminal.StatusError, "Pkg failed to build the binary.")
+		return nil, err
+	}
 
-  u.Step(terminal.StatusOK, "Pkg successfully built the binary.")
+	u.Step(terminal.StatusOK, "Pkg successfully built the binary.")
 
-  name := b.config.Output
-  if (b.config.Output == "") {
-    // The assumed name comes from the package.json file. Read it.
-    // This also assumes that waypoint is being run where the package.json lives
-    // so uh....that should change at some point...
-    packageJsonRaw, err := ioutil.ReadFile("./package.json")
-    if err != nil {
-      u.Step(terminal.StatusError, "An Output was not specified and there was no package.json file")
-      return nil, err
-    }
+	name := b.config.Output
+	if b.config.Output == "" {
+		// The assumed name comes from the package.json file. Read it.
+		// This also assumes that waypoint is being run where the package.json lives
+		// so uh....that should change at some point...
+		packageJsonRaw, err := ioutil.ReadFile("./package.json")
+		if err != nil {
+			u.Step(terminal.StatusError, "An Output was not specified and there was no package.json file")
+			return nil, err
+		}
 
-    packageJson, parseErr := PackageJSON.Parse(packageJsonRaw)
-    if err != nil {
-      return nil, parseErr
-    }
+		packageJson, parseErr := PackageJSON.Parse(packageJsonRaw)
+		if err != nil {
+			return nil, parseErr
+		}
 
-    name = packageJson.Name
-  }
+		name = packageJson.Name
+	}
 
-  // return &Files {
-  //   Path: path.Join("./", name),
-  // }, nil
+	// return &Files {
+	//   Path: path.Join("./", name),
+	// }, nil
 
 	return &Binary{
-    Location: path.Join("./", name),
-  }, nil
+		Location: path.Join("./", name),
+	}, nil
 }
