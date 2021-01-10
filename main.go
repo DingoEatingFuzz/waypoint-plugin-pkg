ffc8142c3da71d2bd9156f4ff5f34f06452d6b3f
Michael Lange <dingoeatingfuzz@gmail.com>
2021-01-10 12:30:32 -0800

    Map the Artifact type to internal Files

--- main.go
+++ main.go
@@ -1,12 +1,26 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/dingoeatingfuzz/waypoint-plugin-pkg/builder"
+	"github.com/dingoeatingfuzz/waypoint-plugin-pkg/registry"
 	sdk "github.com/hashicorp/waypoint-plugin-sdk"
+	"github.com/hashicorp/waypoint/builtin/files"
 )
 
 func main() {
-	sdk.Main(sdk.WithComponents(
-		&builder.Builder{},
-	))
+	sdk.Main(
+		sdk.WithComponents(
+			&builder.Builder{},
+			&registry.Registry{},
+		),
+		sdk.WithMappers(PkgFilesMapper),
+	)
+}
+
+func PkgFilesMapper(src *registry.Artifact) *files.Files {
+	return &files.Files{
+		Path: fmt.Sprintf("%s/%s", src.Host, src.Source),
+	}
 }
