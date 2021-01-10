6e31e469566a512f5bcd3f21d3cff29a23c8dd98
Michael Lange <dingoeatingfuzz@gmail.com>
2021-01-10 12:30:06 -0800

    Basic registry plugin modeled after internal files

--- registry/registry.go
+++ registry/registry.go
@@ -3,14 +3,18 @@ package registry
 import (
 	"context"
 	"fmt"
+	"io"
+	"os"
+	"path"
+	"strings"
 
 	"github.com/dingoeatingfuzz/waypoint-plugin-pkg/builder"
 	"github.com/hashicorp/waypoint-plugin-sdk/terminal"
 )
 
 type RegistryConfig struct {
-	Name    string "hcl:name"
-	Version string "hcl:version"
+	Path string `hcl:"path,attr"`
+	Host string `hcl:"host,attr"`
 }
 
 type Registry struct {
@@ -20,22 +24,6 @@ type Registry struct {
 // Implement Configurable
 func (r *Registry) Config() (interface{}, error) {
 	return &r.config, nil
-}
-
-// Implement ConfigurableNotify
-func (r *Registry) ConfigSet(config interface{}) error {
-	c, ok := config.(*RegistryConfig)
-	if !ok {
-		// The Waypoint SDK should ensure this never gets hit
-		return fmt.Errorf("Expected *RegisterConfig as parameter")
-	}
-
-	// validate the config
-	if c.Name == "" {
-		return fmt.Errorf("Name must be set to a valid directory")
-	}
-
-	return nil
 }
 
 // Implement Registry
@@ -73,9 +61,68 @@ func (r *Registry) PushFunc() interface{} {
 func (r *Registry) push(ctx context.Context, ui terminal.UI, binary *builder.Binary) (*Artifact, error) {
 	u := ui.Status()
 	defer u.Close()
-	u.Update("Pushing binary to registry")
 
-	return &Artifact{}, nil
+	filePath := r.config.Path
+
+	locationParts := strings.Split(binary.Location, "/")
+	name := locationParts[len(locationParts)-1]
+	dest := path.Join(filePath, name)
+
+	u.Update(fmt.Sprintf("Copying binary from %s to %s", binary.Location, dest))
+
+	err := CopyFile(binary.Location, dest)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Artifact{
+		Source: name,
+		Host:   r.config.Host,
+	}, nil
 }
 
-// Implement Authenticator
+// From waypoint internal
+//
+// CopyFile copies the contents of the file named src to the file named
+// by dst. The file will be created if it does not already exist. If the
+// destination file exists, all it's contents will be replaced by the contents
+// of the source file. The file mode will be copied from the source and
+// the copied data is synced/flushed to stable storage.
+func CopyFile(src, dst string) (err error) {
+	in, err := os.Open(src)
+	if err != nil {
+		return
+	}
+	defer in.Close()
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return
+	}
+	defer func() {
+		if e := out.Close(); e != nil {
+			err = e
+		}
+	}()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		return
+	}
+
+	err = out.Sync()
+	if err != nil {
+		return
+	}
+
+	si, err := os.Stat(src)
+	if err != nil {
+		return
+	}
+	err = os.Chmod(dst, si.Mode())
+	if err != nil {
+		return
+	}
+
+	return
+}
